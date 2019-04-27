6b3546376f2ae970415662ad1c4790f0b2c1bc67
Nowina <nowkenaj@op.pl>
2019-04-27 11:03:11 +0200

    added router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
+import { ShopComponent } from './shop/shop.component';
+const routes: Routes = [
+  {path: 'hero', component: HeroDetailsComponent},
+  {path: 'hero-form', component: HeroFormComponent},
+  {path: 'shop', component: ShopComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
